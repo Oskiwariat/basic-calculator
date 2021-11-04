99c4d03be9a3d8066f5ee1ff1299a9f53c8178bf
Oskar <actionman123@interia.pl>
2021-11-05 00:33:20 +0100

    make enviroment ready to work with

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import "../styles/App.css";
+
+import Calculator from "./Calculator";
+
+const App = () => {
+  return (
+    <>
+      <Calculator />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Calculator.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "../styles/Calculator.css";
+
+class Calculator extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>kalkulator</h1>
+      </>
+    );
+  }
+}
+
+export default Calculator;
