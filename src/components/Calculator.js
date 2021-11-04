70738afa038e3858a0625ed3b3fdaa887a3e216d
Oskar <actionman123@interia.pl>
2021-11-05 00:42:47 +0100

    create structure of calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,7 +6,33 @@ class Calculator extends React.Component {
   render() {
     return (
       <>
-        <h1>kalkulator</h1>
+        <div id="calculator">
+          <div id="view">
+            <span id="result">0</span>
+          </div>
+
+          <div id="panel">
+            <div id="numbers">
+              <button>clear</button>
+              <button>1</button>
+              <button>2</button>
+              <button>3</button>
+              <button>4</button>
+              <button>5</button>
+              <button>6</button>
+              <button>7</button>
+              <button>8</button>
+              <button>9</button>
+            </div>
+
+            <div id="operations">
+              <button>-</button>
+              <button>+</button>
+              <button>/</button>
+              <button>=</button>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
