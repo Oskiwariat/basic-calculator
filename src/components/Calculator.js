923f246741bd49b29fe69edcbe1faeea9c4af44c
Oskar <actionman123@interia.pl>
2021-11-05 01:44:39 +0100

    make little calculator design

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -13,23 +13,23 @@ class Calculator extends React.Component {
 
           <div id="panel">
             <div id="numbers">
-              <button>clear</button>
-              <button>1</button>
-              <button>2</button>
-              <button>3</button>
-              <button>4</button>
-              <button>5</button>
-              <button>6</button>
-              <button>7</button>
-              <button>8</button>
-              <button>9</button>
+              <button id="clear">clear</button>
+              <button id="one">1</button>
+              <button id="two">2</button>
+              <button id="three">3</button>
+              <button id="four">4</button>
+              <button id="five">5</button>
+              <button id="six">6</button>
+              <button id="seven">7</button>
+              <button id="eight">8</button>
+              <button id="nine">9</button>
             </div>
 
             <div id="operations">
-              <button>-</button>
-              <button>+</button>
-              <button>/</button>
-              <button>=</button>
+              <button id="minus">-</button>
+              <button id="plus">+</button>
+              <button id="division">/</button>
+              <button id="score">=</button>
             </div>
           </div>
         </div>
